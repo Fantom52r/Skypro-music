eb4ec6ba7e264902e6730d6090717b75d730fdb6
Fantom52r <fantom52rs@gmail.com>
2024-09-13 01:31:52 +0300

    Header.Done

--- app/src/app/components/Header/Header.tsx
+++ app/src/app/components/Header/Header.tsx
@@ -4,28 +4,28 @@ import styles from "./header.module.css"
 const Header = () => {
   return (
     <nav className={styles.mainNav}>
-      <div className="nav__logo logo">
+      <div className={styles.navLogo}>
         <Image  className="logo__image" src="/img/logo.png" alt="logo" height={17} width={113} />
       </div>
-      <div className="nav__burger burger">
-        <span className="burger__line"></span>
-        <span className="burger__line"></span>
-        <span className="burger__line"></span>
+      <div className={styles.navBurger}>
+        <span className={styles.burgerLine}></span>
+        <span className={styles.burgerLine}></span>
+        <span className={styles.burgerLine}></span>
       </div>
-      <div className="nav__menu menu">
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a href="#" className="menu__link">
+      <div className={styles.navMenu}>
+        <ul className={styles.menuList}>
+          <li className={styles.menuItem}>
+            <a href="#" className={styles.menuLink}>
               Главное
             </a>
           </li>
-          <li className="menu__item">
-            <a href="#" className="menu__link">
+          <li className={styles.menuItem}>
+            <a href="#" className={styles.menuLink}>
               Мой плейлист
             </a>
           </li>
-          <li className="menu__item">
-            <a href="../signin.html" className="menu__link">
+          <li className={styles.menuItem}>
+            <a href="../signin.html" className={styles.menuLink}>
               Войти
             </a>
           </li>
